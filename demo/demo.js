1866d474d2880051a5590b44df68b04d810056e8
Tim Fluehr <tfluehr@gmail.com>
2010-08-27 08:42:10 -0400

    demo update.

--- demo/demo.js
+++ demo/demo.js
@@ -6,16 +6,40 @@ function autocompleteUrlDemo(){
     hintMessage: 'This is a simple Auto Complete Textbox List searching/loading data from a url'
   });
 }
+
 function autocompleteLocalDemo(){
   demos.autocompleteLocalDemo = new TextboxList('autocompleteLocalDemo', {
     hintMessage: 'This is a simple Auto Complete Textbox List with it\'s data provided in an array and some values automatically selected'
   }, demoData);
 }
 
+function autocompleteAdvancedDemo(){
+  demos.autocompleteLocalDemo = new TextboxList('autocompleteAdvancedDemo', {
+    maxresults: 4,
+    startsWith: true,
+    secondaryRegExp: '{0}',
+    selectKeys: [{
+      keyCode: Event.KEY_TAB
+    }],
+    customTagKeys: [{
+      character: '"',
+      printable: true,
+      isPair: true
+    }, {
+      character: ' ',
+      printable: true
+    }],
+    noResultsMessage: 'There were no results found matching your search string.',
+    hintMessage: 'This is an advanced Auto Complete Textbox List with it\'s data provided in an array and some values automatically selected.  It has a log of different options set.'
+  }, demoData);
+}
+
 function activateDemos(){
   autocompleteUrlDemo();
   autocompleteLocalDemo();
+  autocompleteAdvancedDemo();
 }
+
 document.observe('dom:loaded', function(){
   new Ajax.Request('../demo/demoData.js', {
     method: 'get',
@@ -23,8 +47,12 @@ document.observe('dom:loaded', function(){
       demoData = transport.responseText.evalJSON(true);
       activateDemos.defer();
       $('submitBtn').observe('click', function(){
-        $H(demos).each(function (tbl){
-          tbl.value.container.next('.SubmitCode').update($F(tbl.value.input)).setStyle({display: 'block'}).previous('.SubmitInfo').update('Form Value:').setStyle({display: 'block'});
+        $H(demos).each(function(tbl){
+          tbl.value.container.next('.SubmitCode').update($F(tbl.value.input)).setStyle({
+            display: 'block'
+          }).previous('.SubmitInfo').update('Form Value:').setStyle({
+            display: 'block'
+          });
         });
       });
     }
