82a24e4cec4d654052c4ea9c4f200d5c696beae7
Tim Fluehr <tfluehr@gmail.com>
2011-01-20 13:02:35 -0500

    changed demo opacity to 1 for examples.

--- demo/demo.js
+++ demo/demo.js
@@ -3,13 +3,15 @@ var demoData = null;
 function autocompleteUrlDemo(){
   demos.autocompleteUrlDemo = new TextboxList('autocompleteUrlDemo', {
     url: '../demo/demoData.js',
-    hintMessage: 'This is a simple Auto Complete Textbox List searching/loading data from a url'
+    hintMessage: 'This is a simple Auto Complete Textbox List searching/loading data from a url',
+    opacity: 1
   });
 }
 
 function autocompleteLocalDemo(){
   demos.autocompleteLocalDemo = new TextboxList('autocompleteLocalDemo', {
-    hintMessage: 'This is a simple Auto Complete Textbox List with it\'s data provided in an array and some values automatically selected'
+    hintMessage: 'This is a simple Auto Complete Textbox List with it\'s data provided in an array and some values automatically selected',
+    opacity: 1
   }, demoData);
 }
 
@@ -30,7 +32,8 @@ function autocompleteAdvancedDemo(){
       printable: true
     }],
     noResultsMessage: 'There were no results found matching your search string.',
-    hintMessage: 'This is an advanced Auto Complete Textbox List with it\'s data provided in an array and some values automatically selected.  It has a log of different options set.'
+    hintMessage: 'This is an advanced Auto Complete Textbox List with it\'s data provided in an array and some values automatically selected.  It has a log of different options set.',
+    opacity: 1
   }, demoData);
 }
 
@@ -45,7 +48,8 @@ function typingListDemo(){
       character: ' ',
       printable: true
     }],
-    hintMessage: 'This list has no auto complete.  It is just a free typing list.'
+    hintMessage: 'This list has no auto complete.  It is just a free typing list.',
+    opacity: 1
   });
 }
 
